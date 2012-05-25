275b38d41052298a320b4f702948b0b94e5a4aa5
Rashit Azizbaev <syndicut@gmail.com>
2012-05-26 02:29:45 +0400

    Added templates

--- js/modules/checkin.js
+++ js/modules/checkin.js
@@ -1,22 +1,12 @@
-define(['jquery', 'modules/api'], function ($, api) {
+define(['jquery', 'modules/api', 'modules/tmpl'], function ($, api, tmpl) {
     // показывает текст "ура, ты зачекинился 123им"
     // и кнопку показать комментарии (позже это будет "авторизироваться")
     // возвращает элемент на который кликать для отображения каментов
     // типо return $('.show-comments');
     return function () {
         api.checkIn(function(data) {
-        /*
-        markup example:
-        <div id="kittycheck-container">
-          <div class="checkin-message">
-            Ура! Ты зачекинился тут
-            <span class="checkin-count">0</span>
-            -м!
-          </div>
-          <div class="show-comments">Показать комментарии</div>
-        </div>
-        */
-        $('#kittycheck-container .checkin-count').text(data.checkins);
+        $('#kittycheck-container').append(tmpl('checkin-message-tmpl', data));
+        $('#kittycheck-container').append('<div class="show-comments">Показать комментарии</div>');
 
         return $('.show-comments');
       });
