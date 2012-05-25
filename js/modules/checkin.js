6050f6e02da3e181766f13ac8d0456d0e697740a
Aleksandr Gornostal <info@sanya.pp.ua>
2012-05-26 00:10:16 +0300

    kittycheck API module

--- js/modules/checkin.js
+++ js/modules/checkin.js
@@ -1,11 +1,11 @@
-define(['jquery'], function () {
+define(['jquery', 'modules/api'], function ($, api) {
     // показывает текст "ура, ты зачекинился 123им"
     // и кнопку показать комментарии (позже это будет "авторизироваться")
     // возвращает элемент на который кликать для отображения каментов
     // типо return $('.show-comments');
     return function () {
-      $.post("/api/checkin/"+window.site_uniq_id, function(data) {
-        $('#kittycheck-container .checkin-count').text(data.checkins);
-      });
+        api.checkIn(function(){
+            $('#kittycheck-container .checkin-count').text(data.checkins);
+        });
     };
 });
