b0235f7729fdd2700790a4d1a35756fb71ff59b0
Grigory Bakunov <thebobuk@ya.ru>
2012-05-25 23:26:47 +0400

    add comments support

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import sys;reload(sys); sys.setdefaultencoding('utf-8');
 sys.path.append('./libs/')
 import hashlib, simplejson, requests, random
+from datetime import datetime
 import pymongo
 from flask import Flask, render_template, request, Response, redirect, abort, session
 from flask.ext.pymongo import PyMongo
@@ -29,12 +30,12 @@ def check_hash_validity(hash):
     except:
         return False
 
-def get_by_hash(hash):
+def get_checkin_by_hash(hash):
     return mongo.db.kitty.find_one({'sitehash': hash})
 
 def get_default_checkin(hash):
     return {
-        'sitehash': hash, "checkins": 1,
+        'sitehash': hash, "checkins": 1, "comments": [],
     }
 
 @app.route('/api/v1/checkin/<sitehash>/', methods = ['POST', 'GET'])
@@ -42,10 +43,10 @@ def api_checkin(sitehash):
     if not check_hash_validity(sitehash):
         return abort(403)
     callback = request.args.get('cb', None)
-    checkin = get_by_hash(sitehash)
+    checkin = get_checkin_by_hash(sitehash)
     if request.method == 'GET':
         if not checkin:
-            return jsonify({'error':"sitehash key not found"})
+            return jsonify({'error':"No checkins yet", "checkins": 0})
     elif request.method == 'POST':
         if not checkin:
             checkin = get_default_checkin(sitehash)
@@ -55,6 +56,27 @@ def api_checkin(sitehash):
             mongo.db.kitty.update({'sitehash': sitehash}, checkin)
     checkin['error'] = 0
     checkin["comments_url"] = '/api/v1/comments/' + sitehash
+    if checkin.has_key("comments"):
+        del checkin["comments"]
+    return jsonify(checkin, callback)
+
+@app.route('/api/v1/comments/<sitehash>/', methods = ['POST', 'GET'])
+def api_comments(sitehash):
+    if not check_hash_validity(sitehash):
+        return abort(403)
+    callback = request.args.get('cb', None)
+    checkin = get_checkin_by_hash(sitehash)
+    if request.method == 'GET':
+        if not checkin or not checkin.has_key('comments') or len(checkin['comments']) == 0:
+            return jsonify({'error':"No comments yet", "checkins": 0, "comments": []})
+    elif request.method == 'POST':
+        text = request.form.get('text', '')
+        if not checkin:
+            checkin = get_default_checkin(sitehash)
+            mongo.db.kitty.insert(checkin)
+        checkin['comments'].append({"text": text, "author": 'nobody', "datetime": str(datetime.now())})
+        mongo.db.kitty.update({'sitehash': sitehash}, checkin)
+    checkin['error'] = 0
     return jsonify(checkin, callback)
 
 if __name__ == "__main__":
