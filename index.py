148cb75b8c156748b71f2846aed951ece5f1beef
Grigory Bakunov <thebobuk@ya.ru>
2013-07-16 23:35:00 +0400

    update to 1.1

--- index.py
+++ index.py
@@ -22,7 +22,7 @@ twitter = oauth.remote_app('KittyCheck',
     consumer_key=config.TWITTER["consumer_key"],
     consumer_secret=config.TWITTER["consumer_secret"],
 
-    base_url='http://api.twitter.com/1/',
+    base_url='http://api.twitter.com/1.1/',
     request_token_url='http://api.twitter.com/oauth/request_token',
     access_token_url='http://api.twitter.com/oauth/access_token',
     authorize_url='http://api.twitter.com/oauth/authenticate',
